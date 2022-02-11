211b50e825b2bcc7067c0bfb97caf441bd3b81d0
baebumsoo <baebeomsu77@gmail.com>
2022-02-11 18:51:37 +0900

    commit

--- 2.Clock/clock.js
+++ 2.Clock/clock.js
@@ -1,3 +1,18 @@
 const secondHand = document.querySelector(".second-hand");
-const minsHand = document.querySelector(".min-hand");
+const minsHand = document.querySelector(".minute-hand");
 const hourHand = document.querySelector(".hour-hand");
+
+function setTime() {
+  const today = new Date();
+  const hours = today.getHours();
+  const minutes = today.getMinutes();
+  const seconds = today.getSeconds();
+
+  hourHand.style.transform = `rotate(${hours * 30}deg)`;
+  minsHand.style.transform = `rotate(${minutes * 6} deg)`;
+  secondHand.style.transform = `rotate(${seconds * 6}deg)`;
+}
+
+setInterval(setTime, 1000);
+
+setTime();
